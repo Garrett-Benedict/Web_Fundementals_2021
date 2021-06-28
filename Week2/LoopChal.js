1c98332297093c3331b8114753a496bccac77127
Garrett Benedict <garrett.benedict.080@gmail.com>
2021-06-28 15:22:31 -0700

    Week 2

---
+++ Week2/LoopChal.js
@@ -0,0 +1,23 @@
+for(i=1 ; i < 21 ; i+=2){
+    console.log(i);
+}
+
+for(i=100 ; i > 2 ; i--){
+    if(i % 3 == 0) console.log(i);
+}
+
+for(i=4 ; i < -4 ; i-=1.5){
+    console.log(i);
+}
+
+var sumofNum=0;
+for(i=1 ; i <= 100 ; i++){
+    sumofNum = sumofNum + i;
+}
+console.log(sumofNum);
+
+var productofNum=1;
+for(i=1 ; i <= 12; i++){
+    productofNum = productofNum * i;
+}
+console.log(productofNum);
