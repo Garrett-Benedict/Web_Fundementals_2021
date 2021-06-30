c18b1da27cbd8d23a3fbe58e15c4721a3fefba25
Garrett Benedict <garrett.benedict.080@gmail.com>
2021-06-29 20:31:27 -0700

    Week 2

---
+++ Week2/Likes_Garrett_Benedict/likes2.js
@@ -0,0 +1,24 @@
+
+    var likes1 = 9;
+function btn1() {
+    var btn1 = document.querySelector(".btn-1");
+    likes1++;
+    btn1.innerText = likes1 + "like(s)";
+}
+
+
+
+var likes2 = 12;
+function btn2() {
+    var btn2 = document.querySelector(".btn-2")
+    likes2++;
+    btn2.innerText = likes2 + "like(s)";
+}
+
+
+var likes3 = 9;
+function btn3() {
+    var btn3 = document.querySelector(".btn-3");
+    likes3++;
+    btn3.innerText = likes3 + "like(s)";
+} 
